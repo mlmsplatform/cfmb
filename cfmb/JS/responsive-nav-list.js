32a27abb7c2541365a583305a2626f8353a41e10
MLMS Platform <111484608+mlmsplatform@users.noreply.github.com>
2023-06-14 00:10:12 +0545

    Delete cfmb directory

--- cfmb/JS/responsive-nav-list.js
+++
@@ -1,6 +0,0 @@
-function onClickFunction(){
-    document.querySelector(".menu").classList.toggle("cross");
-    // console.log("doc",document.querySelector(".menu"));
-    // console.log("doc",document.querySelector(".nav-list"))
-    document.querySelector(".nav-list").classList.toggle("nav-list-shown");
-}
